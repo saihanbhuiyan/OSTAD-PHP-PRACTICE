f273a2689f685cbfbaadea120c8175af07f788ba
saihan <saihanbhuiyan549@gmail.com>
2024-05-02 17:27:25 +0600

    6.1 printf

---
+++ module-6.1/printf.php
@@ -0,0 +1,15 @@
+<?php 
+
+// printf
+
+echo  "what is printf"."<br>";
+echo "printf hocche ikti build in functio ja echo we moto kaj kore pasapasi variable o declar kore %d for decimal and %s for string";
+echo "<br>";
+$name = "saihan";
+$age = 25;
+printf("name = %s age = %d", $name, $age);
+echo"<br>";
+printf("student name is %s and age is:%d",$name , $age);
+
+
+?>
