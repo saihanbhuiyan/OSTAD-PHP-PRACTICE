8495cd4dcec06cbbe6eaddfd0f21c067fa46aed3
saihan <saihanbhuiyan549@gmail.com>
2024-05-19 01:37:31 +0600

    functoin with params

---
+++ module-7.1/functionWithParams.php
@@ -0,0 +1,19 @@
+<?php 
+// what is argument?
+// *os-->functiona a bahir theke kono value pass korbo ba throw korbo ba dhukabo akhane j value gulu k dhukabo agulu holo argument
+
+// What is parameter?
+// *j value gulu bahir theke asbe se valu gulu k jar maddome catch korbo ba rcv kore function a appy korbo tai hocche parameter
+
+//-->function k jekhantheke call korbe function sekhan theke e execute hobe like(function er bahire kothaw function k call korle j line diye funciton k call koreche ba jekhan theke call koreche functon sekhan theke execute hoye trpr function er vitor jabe totha argument valu niye trpr function a jabe)
+// ---->totha argument niye trpr funcitona a dhukbe and paramet argument valu k rcv korbe
+
+
+function sum($a,$b){
+    echo $a+$b;
+}
+
+sum(20,30);
+
+
+?>
