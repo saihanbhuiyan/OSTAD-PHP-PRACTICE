150569cce211db1645197d3cb12a09f837351bdf
saihan <saihanbhuiyan549@gmail.com>
2024-05-19 17:00:25 +0600

    bahirer variable access ba scope in function by using paramiter and 2nd global

---
+++ module-7.1/variableScopeInFunction.php
@@ -0,0 +1,29 @@
+<?php
+// what is variable scope 
+// ** 1. function er bahire j variable ase ta function er vitore kaj korena atai variable scope ja nirdharon kore konti access korbe konti korbena 
+// ** 2. amra 2vabe bahire variable k function er vitore access korte pari
+// number 1)==>function er vitore peramiter pass kore access korte pari ja local hoyejabe access er por
+
+// example variable scope in function 1way
+  $name = "saihan";
+  $age = 22;
+  function greed($n, $a){
+    echo "my name is $n and my age is $a paramiter";
+  }
+  greed($name,$age);
+  echo"<br>";
+
+// number 2)==>function er vitore global use kore bahirer variable k function er vitore ese access korte pari ba use korte pari (ja bad habid ostad boleche)
+
+// example variable scope in function 2way
+
+$n = "raihan";
+$a = 30;
+function myfunction(){
+    global $n;
+    global $a;
+    echo "my name is $n and my age is $a global";
+}
+myfunction();
+
+
