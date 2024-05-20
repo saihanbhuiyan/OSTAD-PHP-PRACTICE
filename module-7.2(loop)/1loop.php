b3153d560fe66a3b4d099c8cb5a80e133802466d
saihan <saihanbhuiyan549@gmail.com>
2024-05-20 11:39:44 +0600

    7.1er topic wise serial list

---
+++ module-7.2(loop)/1loop.php
@@ -0,0 +1,12 @@
+<?php
+// what is loop?
+// ** ekti particular ongso k bar bar chalano e hocche loop.
+
+// kind of loop:- loop sadharonoto 4 prokar
+// 1. for loop
+// 2. while loop
+// 3. do while loop
+// 4. foreach loop
+
+// ader modde for loop abong foreach loop sobtheke beshe bebohar hoye thake
+//*** ==>foreach loop maximun khetre array er jonno bebohar hoye thake */
