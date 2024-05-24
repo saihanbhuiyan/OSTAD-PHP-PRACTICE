08cc4591c252283b32859613a5d3cbff78254c12
saihan <saihanbhuiyan549@gmail.com>
2024-05-24 23:49:46 +0600

    what is array and trype of array

---
+++ module-8.1(array)/array.php
@@ -0,0 +1,9 @@
+<?php
+// what is array?
+// ** array hocche ekti order map ja aki typer onekgulu value akti single ekti single variable a thake
+//  -->shortly(array is a collection of data in a single variable )
+
+// there have tree type of array
+// 1. indexed array
+// 2. associative array
+// 3. multi dimensional array
