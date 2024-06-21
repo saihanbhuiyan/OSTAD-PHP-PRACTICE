966db30596f7e8a6f56af283cb97fbb56343f24f
saihan <saihanbhuiyan549@gmail.com>
2024-06-22 00:43:49 +0900

    string theke index wise latter ba character newa array er moto

--- module-8.2(string manipulation)/1stringGetCharacterIndexWiseTreatAsArray.php
+++ module-8.2(string manipulation)/1stringGetCharacterIndexWiseTreatAsArray.php
@@ -6,3 +6,10 @@
 // string k array er moto treat:
    $str = "hello Saihan";
    echo $str[0]."<br>";
+   echo $str[1]."<br>";
+   echo $str[2]."<br>";
+   echo $str[3]."<br>";   //Out Put :- h e l l 0
+   echo $str[4]."<br>";      
+   echo $str[5]."<br>";
+
+   
