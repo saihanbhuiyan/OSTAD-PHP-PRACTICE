9acde49843f49a262dd32b854a308ffc809b873d
saihan <saihanbhuiyan549@gmail.com>
2024-06-22 00:41:43 +0900

    string theke index wise latter newa array er moto

---
+++ module-8.2(string manipulation)/1stringGetCharacterIndexWiseTreatAsArray.php
@@ -0,0 +1,8 @@
+<?php
+
+// ----------------->string manipulation<------------------
+// string alpha new merical character hote pare A to Z abong new-merical character 1-10
+
+// string k array er moto treat:
+   $str = "hello Saihan";
+   echo $str[0]."<br>";
