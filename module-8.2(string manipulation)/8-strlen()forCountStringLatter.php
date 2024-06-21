0c760f3ce4111270275da386615e8194e39fb1b4
saihan <saihanbhuiyan549@gmail.com>
2024-06-22 02:27:32 +0900

    strlen() for count each character of a string

---
+++ module-8.2(string manipulation)/8-strlen()forCountStringLatter.php
@@ -0,0 +1,9 @@
+<?php
+//strlen() for count string latter
+//---->kono string a koto gulu latter ase ta count korte use hoi strlen() function
+
+//count strin
+
+$name = "saihan abdulla";
+
+echo strlen($name); //output : 14
