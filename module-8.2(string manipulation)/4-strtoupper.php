494e06f943ca08134ba07421cade7bf35d9aa569
saihan <saihanbhuiyan549@gmail.com>
2024-06-22 01:13:49 +0900

    string k uppercase ba borohater character a convert korbe

--- module-8.2(string manipulation)/4-strtoupper.php
+++ module-8.2(string manipulation)/4-strtoupper.php
@@ -2,6 +2,8 @@
 //strtoupper use korahoi jekono string k upper case ba boro latter a convert korte
 
 $str = "hello saihan";
-echo strtoupper($str);
+echo strtoupper($str)."<br>";
+$newstr = strtoupper($str);
+echo $newstr;
 ?>
 <!-- avabe amra upper case a convert korte pari  --> 
