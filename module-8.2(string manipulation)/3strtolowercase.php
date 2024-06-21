396ff4dfc6a129c2ad4cced1017b4a8dbd73fa46
saihan <saihanbhuiyan549@gmail.com>
2024-06-22 01:00:26 +0900

    string k loware case ba choto latter ba character a convert korte use hoi

---
+++ module-8.2(string manipulation)/3strtolowercase.php
@@ -0,0 +1,7 @@
+<?php
+// strtolower use kore amra string er case change korte pari
+// --->j kono string k choto character a convert korar jonno use hoi
+
+$str = "HELLO WORLD";
+echo strtolower($str);
+?>
