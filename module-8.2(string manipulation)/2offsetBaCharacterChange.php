c259402f796edab52b58f30e20a1ea4ab558c798
saihan <saihanbhuiyan549@gmail.com>
2024-06-22 00:52:29 +0900

    index number dhore dhore character replace ba change kora array er moto

---
+++ module-8.2(string manipulation)/2offsetBaCharacterChange.php
@@ -0,0 +1,10 @@
+<?php
+//array er moto index number diye change korte pari
+//jevebe find koreche oivabe e just ki diye replace korbo oi index number k tai bole dite hobe
+
+$str = "hello saihan";
+ $str[0] = "O";
+ $str[1] = "o"; 
+echo $str;  //out put : Oollo saihan
+
+//avabe amra replace ba index wise change korte pari
