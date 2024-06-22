6cb9a2d6969d948647bf4d25405115b419cf71d2
saihan <saihanbhuiyan549@gmail.com>
2024-06-23 02:37:06 +0900

    shuffle()use kore array element elomelo kortepari(we can creat lottery using shuffle)

---
+++ module-8.2(string manipulation)/array(shuffle,filter,onlySelectMaleInMultipleArray,array_map)/shuffle()ArrayElomeloKorteUseHoi.php
@@ -0,0 +1,7 @@
+<?php
+// shuffle() buildin function use kore kono array k elomelo korar jonno use hoi;
+// **** shuffle use kore game o creat korte pari like kar nam ba number age ase luck funder
+
+$array = ["red", "green", "blue", "yellow"];
+shuffle($array);
+print_r($array);
