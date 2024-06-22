9d86e165af7ea0d51dc0b4ba79abc381e3a1096c
saihan <saihanbhuiyan549@gmail.com>
2024-06-23 02:12:57 +0900

    str_ireplacae(old,new,variable)use kore oi sokol same word replace korahoi notun word diye

---
+++ module-8.2(string manipulation)/20-str_ireplace(old,new,vari)i-replaceKorle-oiWordTiStringA-jotojaigaiAseSobChangeHoyeJabe.php
@@ -0,0 +1,8 @@
+<?php
+// ***str_ireplace() function use kore string a kono word k replace korle oi string a same
+// ---->j kono joto word ase sob change hoyejabe totha replace hoyejabe
+// **ajonno prothome str_ireplace($old,$new,$str) diye then j old wolrd ti replace korba setha then j new word ti replace korba seta 
+// --->then j string ti replace korba seta
+
+$string = "hello saihan i have seen is first time,this is a beautiful name saihan";
+echo str_ireplace("saihan", "abdullah", $string);
