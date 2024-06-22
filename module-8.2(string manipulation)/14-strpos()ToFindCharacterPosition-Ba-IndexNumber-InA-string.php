06b0b22a3dedac5a6376a6fb9ec4be56e6b7dbc6
saihan <saihanbhuiyan549@gmail.com>
2024-06-22 17:57:05 +0900

    strpose(variable,character)prothome variable then j character er position find korbo seta

---
+++ module-8.2(string manipulation)/14-strpos()ToFindCharacterPosition-Ba-IndexNumber-InA-string.php
@@ -0,0 +1,9 @@
+<?php
+// strpos() use korahoi kono string er protiti character er index number ba position return korbe(ba ber korar jonno)
+// ***--->ajonno strpose er vitor string $Variable, then se string er kon character er index nuumber jante cacche se character
+
+
+// find string index number
+$name = "saihan vaya i am comming tommorrow";
+$position = strpos($name,"g");
+echo $position;
