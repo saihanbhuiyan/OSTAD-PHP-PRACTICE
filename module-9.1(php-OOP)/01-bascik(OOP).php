38f0f22a4f56d1c53a1c180aab2883f1382f98b1
saihan <saihanbhuiyan549@gmail.com>
2024-06-23 13:27:41 +0900

    oop ki o keno?

---
+++ module-9.1(php-OOP)/01-bascik(OOP).php
@@ -0,0 +1,10 @@
+<?php
+// what is oop?
+// Ans: oop is a concept of object oriented programming
+// --->code guluke sajiye likhar ekti way, code likhar system kmn hobe ta boledi
+// ---->code gulu k kivabe sonnibesh korbo setar ekti standard way hocche oop.
+
+// Why oop?
+// -->oop hocche ekti platform ba akti rules ja mantain kore code likh sokol developer der jonno bodgommo hobe
+// --->ekti code jeno bar bar na likhte hoi..totha ekti code jeno bar bar reuse juggo hoi..
+// ---->tai oop follow korahoi..
